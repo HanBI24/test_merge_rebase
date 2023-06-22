cbc90fa208b77824a9ce069b91f5ecb5fc0dfdc9
HanBI24 <alsltjdrhs@naver.com>
2023-06-22 11:03:05 +0900

    Add dev branch

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// dev
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
